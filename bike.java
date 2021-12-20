8a92f433ad50ff695aee2896ff629f8a80a52de4
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-20 23:00:00 +0530

    add the bike file on 20dec 20

---
+++ bike.java
@@ -0,0 +1,21 @@
+class yamahaRX135{
+	public static void main(String[] args){
+		byte milage = 35;
+	System.out.println(milage);
+		short maxpower = 14;
+	System.out.println(maxpower);
+		int engine = 132;
+	System.out.println(engine);
+		long price = 400000000000l;
+	System.out.println(price);
+		float torque = 12.25f;
+	System.out.println(torque);
+		double fuletankcapacity = 10.5111111111111;
+	System.out.println(fuletankcapacity);
+		boolean fivegears = true;
+	System.out.println(fivegears);
+		char symbol = 'y';
+	System.out.println(symbol);
+		
+}
+}
